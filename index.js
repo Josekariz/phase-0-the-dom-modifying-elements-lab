bcc5cbb19dbe8e83edf032c9c55a1a0f56bffa19
Josekariz <joseph.macharia@student.moringaschool.com>
2022-08-04 21:29:50 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementById("main")
+element.remove();
+
+var newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Joseph is the champion"
